71bd5713fca5621cb509568023dc9f0d629c2dae
TomaszA2 <35873122+TomaszA2@users.noreply.github.com>
2018-03-08 19:38:26 +0100

    Created

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+import discord
+import asyncio
+global str
+
+client = discord.Client()
+szukane="stand"
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.author.nick == "Joseph":
+        tmp = await client.send_message(message.channel, 'Yare Yare Daze.')
+
+client.run(os.getenv('TOKEN'))
